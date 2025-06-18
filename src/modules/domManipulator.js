739165b94ab293ffb0d88218a9507ef61ac0cedf
JBeck5 <73002731+JBeck5@users.noreply.github.com>
2025-06-18 13:28:09 -0700

    added window closes for project modal

--- src/modules/domManipulator.js
+++ src/modules/domManipulator.js
@@ -1,12 +1,30 @@
 //Manipulates the DOM
+const modal = document.querySelectorAll("modal");
+const projectModal = document.getElementById("project-modal");
+const projectContent = document.getElementById("modal-content");
+const projectForm = document.getElementById("projectForm");
+const newProjectBtn = document.getElementById("btn-new-project");
+const closeProjectModal = document.getElementById("close-project-modal");
+const cancelBtn = document.getElementById("btn-cancel-project");
+
 
 export function initializeModal() {
-    const projectModal = document.getElementById("project-modal");
-    const newProjectBtn = document.getElementById("btn-new-project");
-
-    console.log("HERE");
-
-    newProjectBtn.addEventListener("click", function () {
-        projectModal.style.display = "block";
+    newProjectBtn.addEventListener("click", function() {
+        projectModal.style.display = "flex";
     })
+
+    cancelBtn.addEventListener("click", closeProjectWindow)
+
+    closeProjectModal.addEventListener("click", closeProjectWindow)
+
+    window.onclick = function(event) {
+        if(event.target == projectModal) {
+            closeProjectWindow();
+        }
+    }
 };
+
+function closeProjectWindow() {
+        projectModal.style.display = "none";
+        projectForm.reset();
+}
