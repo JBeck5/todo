12e1368e79635dfb5299d8d5dbbe145478dd965a
JBeck5 <73002731+JBeck5@users.noreply.github.com>
2025-06-25 13:22:10 -0700

    Completed for Testing

--- src/modules/projectManager.js
+++ src/modules/projectManager.js
@@ -1,17 +1,39 @@
+//Handles and Manages Projects
+const STORAGE_KEY = "projects";
 
-
-function createProject {
+class ProjectManager {
+    constructor() {
+        this.projects = this.loadProjects();
+    }
     
+    saveProjects() {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(this.projects));
+    }
+
+    loadProjects() {
+        const loadedProjects = localStorage.getItem(STORAGE_KEY);
+        return loadedProjects ? JSON.parse(loadedProjects) : [];
+    }
+
+    getProjects() {
+        return this.projects;
+    }
+
+    addProject(name) {
+        const project = {
+            id: crypto.randomUUID(),
+            name: name,
+            tasks: [],
+        };
+        this.projects.push(project);
+        this.saveProjects();
+        return project;
+    }
+
+    removeProject(id) {
+        this.projects = this.projects.filter((project) => project.id !== id);
+        this.saveProjects();
+  }
 }
 
-function openProject {
-
-}
-
-function deleteProject {
-
-}
-
-function editProject {
-
-}
+export default new ProjectManager();
